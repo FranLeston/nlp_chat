92b23a5df63f01b7feb877bc521f9573f61384df
Fran Leston <lestonramos@gmail.com>
2021-05-02 23:53:47 +0200

    heroku

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ load_dotenv()
 
 # My functions
 app = Flask(__name__, static_folder='client/dist/', static_url_path='/')
-app.config.from_object(__name__)
+app.config.from_object(__name__) 
+
 
 if __name__ == '__main__':
     conn = db.connect_to_mysql()
