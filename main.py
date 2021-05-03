69081677913abd4cbad6ce8cec2f1860c9c8a7a1
Fran Leston <lestonramos@gmail.com>
2021-05-03 08:59:39 +0200

    heroku

--- main.py
+++ main.py
@@ -16,27 +16,30 @@ from flask_cors import CORS
 load_dotenv()
 
 # My functions
-app = Flask(__name__, static_folder='client/dist/', static_url_path='/')
-app.config.from_object(__name__) 
-
+#app = Flask(__name__, static_folder='client/dist/', static_url_path='/')
+# app.config.from_object(__name__)
+app = Flask(__name__)
 
 conn = db.connect_to_mysql()
 if conn:
     db.create_schemas(conn)
 
 
-
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 # Set up the index route
+
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
+
 app.register_blueprint(UserRoutes)
 app.register_blueprint(ChatRoutes)
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port,threaded=True)
-#app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port, threaded=True)
+    app.run()
